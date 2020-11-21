28936c1cba6c73b2c00e775b6b050e0a146e1595
thanapon072 <42941804+thanapon072@users.noreply.github.com>
2020-11-21 22:55:49 +0700

    Animatedที่มึงให้กูรีเวิทอะสัส

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-window.onscroll = function(){scrollFunction()};
-console.log(window.pageYOffset)
-function animation1(){
-    
+window.onscroll = function() {
+    console.log(document.body.scrollTop)
+    console.log(document.documentElement.scrollTop)
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.documentElement.scrollTop > 2600) {
+        document.getElementById("mariam_p2").style.left = 48 + '%'
+        document.getElementById("plastic_bag").style.left = 15 + '%'
+    } else {
+        document.getElementById("mariam_p2").style.left = 53 + '%'
+        document.getElementById("plastic_bag").style.left = 8 + '%'
+    }
 }
