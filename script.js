c7d7250313439fade76646794a7f2d06255b4c2b
thanapon072 <42941804+thanapon072@users.noreply.github.com>
2020-11-21 22:52:26 +0700

    Revert "animated"
    This reverts commit e0e120a9bbf1cb7d357b4d892042e86b4e068c0c.

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-window.onscroll = function() {
-    console.log(document.body.scrollTop)
-    console.log(document.documentElement.scrollTop)
-    scrollFunction()
-};
-
-function scrollFunction() {
-    if (document.documentElement.scrollTop > 2600) {
-        document.getElementById("mariam_p2").style.left = 48 + '%'
-        document.getElementById("plastic_bag").style.left = 15 + '%'
-    } else {
-        document.getElementById("mariam_p2").style.left = 53 + '%'
-        document.getElementById("plastic_bag").style.left = 8 + '%'
-    }
+window.onscroll = function(){scrollFunction()};
+console.log(window.pageYOffset)
+function animation1(){
+    
 }
