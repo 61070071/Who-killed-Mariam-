eb13e8c453584c7bb2189a23fd10725fe841c354
jaynhai <thanapon.asd@gmail.com>
2020-11-23 21:03:45 +0700

    graph animated

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onscroll = function() {
     console.log(document.body.scrollTop)
     console.log(document.documentElement.scrollTop)
     scrollFunction()
+    scrollGraph()
 };
 
 function scrollFunction() {
@@ -11,6 +12,35 @@ function scrollFunction() {
     } else {
         document.getElementById("mariam_p2").style.left = 53 + '%'
         document.getElementById("plastic_bag").style.left = 8 + '%'
+    }
+}
+function scrollGraph() {
+    if (document.documentElement.scrollTop > 7600) {
+        document.getElementById("g_1").style.height = 17 + 'vw'
+        document.getElementById("g_2").style.height = 25 + 'vw'
+        document.getElementById("g_3").style.height = 19 + 'vw'
+        document.getElementById("g_4").style.height = 26.5 + 'vw'
+        document.getElementById("g_5").style.height = 34 + 'vw'
+        document.getElementById("g_6").style.height = 19 + 'vw'
+        document.getElementById("g_7").style.height = 19 + 'vw'
+        document.getElementById("g_8").style.height = 21 + 'vw'
+        document.getElementById("g_9").style.height = 22 + 'vw'
+        document.getElementById("g_10").style.height = 36 + 'vw'
+        document.getElementById("g_11").style.height = 27 + 'vw'
+        document.getElementById("scale_y").style.height = 31 + 'vw'
+    } else {
+        document.getElementById("g_1").style.height = 5+ 'vw'
+        document.getElementById("g_2").style.height = 5 + 'vw'
+        document.getElementById("g_3").style.height = 5 + 'vw'
+        document.getElementById("g_4").style.height = 5 + 'vw'
+        document.getElementById("g_5").style.height = 5 + 'vw'
+        document.getElementById("g_6").style.height = 5 + 'vw'
+        document.getElementById("g_7").style.height = 5 + 'vw'
+        document.getElementById("g_8").style.height = 5 + 'vw'
+        document.getElementById("g_9").style.height = 5 + 'vw'
+        document.getElementById("g_10").style.height = 5 + 'vw'
+        document.getElementById("g_11").style.height = 5 + 'vw'
+        document.getElementById("scale_y").style.height = 5 + 'vw'
     }
 }
 let count = 0
