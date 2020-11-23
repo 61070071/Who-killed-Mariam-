7fe9ac3a52dc16e42ce5317486e0d8bd2e576828
61070071 <61070071@kmitl.ac.th>
2020-11-23 17:32:53 +0700

    หน้า 6 ส่วนเพิ่มเติม

--- script.js
+++ script.js
@@ -13,3 +13,39 @@ function scrollFunction() {
         document.getElementById("plastic_bag").style.left = 8 + '%'
     }
 }
+let count = 0
+function change_page6(num){
+    count += num
+    if (count == 0) {
+        p6_page1.style.opacity = "1"
+        p6_page2.style.opacity = "0"
+        p6_page3.style.opacity = "0"
+        p6_page1.style.left = "0%"
+        p6_page2.style.left = "100%"
+        p6_page3.style.left = "100%"
+        r_arrow_p6.style.display = "block"
+        l_arrow_p6.style.display = "none"
+
+    }
+    if (count == 1) {
+        p6_page1.style.opacity = "0"
+        p6_page2.style.opacity = "1"
+        p6_page3.style.opacity = "0"
+        p6_page1.style.left = "-100%"
+        p6_page2.style.left = "0%"
+        p6_page3.style.left = "100%"
+        r_arrow_p6.style.display = "block"
+        l_arrow_p6.style.display = "block"
+    }
+    if (count == 2) {
+        p6_page1.style.opacity = "0"
+        p6_page2.style.opacity = "0"
+        p6_page3.style.opacity = "1"
+        p6_page1.style.left = "-100%"
+        p6_page2.style.left = "-100%"
+        p6_page3.style.left = "0%"
+        r_arrow_p6.style.display = "none"
+        l_arrow_p6.style.display = "block"
+    }
+    
+}
